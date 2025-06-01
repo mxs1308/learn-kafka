c81b6176be139283fcb54bbe5ca4586e396b921c
mxs1308 <mayank.shandilya1308@gmail.com>
2025-06-01 19:28:18 +0530

    Initial Commit

---
+++ kafka-basics/src/test/java/com/basics/kafka/KafkaBasicsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.basics.kafka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KafkaBasicsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
